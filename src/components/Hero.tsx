a7ddf49d21ddd74237acb3187eeac482e46fa7bd
Salles <gestoreosalles@gmail.com>
2026-01-06 22:31:35 -0300

    Primeiro commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,186 @@
+import { ArrowRightIcon, PlayIcon, ZapIcon, CheckIcon } from 'lucide-react';
+import { PrimaryButton, GhostButton } from './Buttons';
+import { motion } from 'framer-motion';
+
+export default function Hero() {
+
+    const trustedUserImages = [
+        'https://media-gru1-2.cdn.whatsapp.net/v/t61.24694-24/484883790_9761633373870284_2027230454231440722_n.jpg?ccb=11-4&oh=01_Q5Aa3gHlThr-rHJCYWcVk33usJyFXWzmvAo4L8uEv4xvemo0Vw&oe=696AC1CC&_nc_sid=5e03e0&_nc_cat=101w=50',
+        'https://media-gru1-2.cdn.whatsapp.net/v/t61.24694-24/588568607_1570195707662853_186573069393543202_n.jpg?ccb=11-4&oh=01_Q5Aa3gGNsJdGjwpKAUisUzn8MbdTMUzEQeKaudneWe6Xmi_z9Q&oe=696AC6B6&_nc_sid=5e03e0&_nc_cat=107w=50',
+        'https://pps.whatsapp.net/v/t61.24694-24/309221542_658208598979902_6592391754799322477_n.jpg?ccb=11-4&oh=01_Q5Aa3gHBVYtZ7D3J2eumY2kA9kpASmAIuqbuEBb-m-iDOPSMsg&oe=696A7E22&_nc_sid=5e03e0&_nc_cat=105w=50&h=50&fit=crop'
+    ];
+
+    const mainImageUrl = 'https://mkt.purplecsc.com/wp-content/uploads/2025/06/Group-10-7-1.png?q=80&w=1600&auto=format&fit=crop';
+
+    const trustedLogosText = [
+        'Rotha Fitness',
+        'Tennis Lounge',
+        'Beach Arena',
+        'Riplay Sports',
+        'Ponto Zero',
+        'Up Fit Gym',
+        'Athletica Pro',
+    ];
+
+    return (
+        <>
+            <section id="home" className="relative z-10">
+                <div className="max-w-6xl mx-auto px-4 min-h-screen max-md:w-screen max-md:overflow-hidden pt-32 md:pt-26 flex items-center justify-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+                        <div className="text-left">
+                            <motion.a href="https://prebuiltui.com/tailwind-templates?ref=pixel-forge" className="inline-flex items-center gap-3 pl-3 pr-4 py-1.5 rounded-full bg-white/10 mb-6 justify-start"
+                                initial={{ y: 60, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true }}
+                                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
+                            >
+                                <div className="flex -space-x-2">
+                                    {trustedUserImages.map((src, i) => (
+                                        <img
+                                            key={i}
+                                            src={src}
+                                            alt={`Client ${i + 1}`}
+                                            className="size-6 rounded-full border border-black/50"
+                                            width={40}
+                                            height={40}
+                                        />
+                                    ))}
+                                </div>
+                                <span className="text-xs text-gray-200/90">
+                                    Marketing & Comunicação simplesmente criativa!
+                                </span>
+                            </motion.a>
+
+                            <motion.h1 className="text-4xl md:text-5xl font-bold leading-tight mb-6 max-w-xl"
+                                initial={{ y: 60, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true }}
+                                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.1 }}
+                            >
+                                Seu marketing precisa de um time. <br />
+                                <span className="bg-clip-text text-transparent bg-linear-to-r from-indigo-300 to-indigo-400">
+                                    Seu negócio precisa de resultados.
+                                </span>
+                            </motion.h1>
+
+                            <motion.p className="text-gray-300 max-w-lg mb-8"
+                                initial={{ y: 60, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true }}
+                                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.2 }}
+                            >
+                                Purple é marketing e comunicação feitos de forma simples, criativa e estratégica.
+                                Aqui, seu negócio não anda sozinho: você ganha um time que pensa, executa e entrega resultados de verdade.
+                            </motion.p>
+
+                            <motion.div className="flex flex-col sm:flex-row items-center gap-4 mb-8"
+                                initial={{ y: 60, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true }}
+                                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.3 }}
+                            >
+                                <a href="/" className="w-full sm:w-auto">
+                                    <PrimaryButton className="max-sm:w-full py-3 px-7">
+                                        Beleza, como funciona?
+                                        <ArrowRightIcon className="size-4" />
+                                    </PrimaryButton>
+                                </a>
+
+                                <GhostButton className="max-sm:w-full max-sm:justify-center py-3 px-5">
+                                    <PlayIcon className="size-4" />
+                                    Saiba mais sobre nós
+                                </GhostButton>
+                            </motion.div>
+
+                            <motion.div className="flex sm:inline-flex overflow-hidden items-center max-sm:justify-center text-sm text-gray-200 bg-white/10 rounded"
+                                initial={{ y: 60, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                viewport={{ once: true }}
+                                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.1 }}
+                            >
+                                <div className="flex items-center gap-2 p-2 px-3 sm:px-6.5 hover:bg-white/3 transition-colors">
+                                    <ZapIcon className="size-4 text-sky-500" />
+                                    <div>
+                                        <div>Execução orientada por estratégia</div>
+                                        <div className="text-xs text-gray-400">
+                                            Focado em crescimento e resultados.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="hidden sm:block h-6 w-px bg-white/6" />
+
+                                <div className="flex items-center gap-2 p-2 px-3 sm:px-6.5 hover:bg-white/3 transition-colors">
+                                    <CheckIcon className="size-4 text-cyan-500" />
+                                    <div>
+                                        <div>Serviço de entrega completo</div>
+                                        <div className="text-xs text-gray-400">
+                                            Design, desenvolvimento e marketing
+                                        </div>
+                                    </div>
+                                </div>
+                            </motion.div>
+                        </div>
+
+                        {/* Right: modern mockup card */}
+                        <motion.div className="mx-auto w-full max-w-lg"
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            viewport={{ once: true }}
+                            transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.5 }}
+                        >
+                            <motion.div className="rounded-3xl overflow-hidden bg-transparent">
+                                <div className="relative bg-transparent">
+                                    <img
+                                        src={mainImageUrl}
+                                        alt="antonio-home"
+                                        className="w-full h-full object-cover object-center"
+                                    />
+                                </div>
+                            </motion.div>
+
+                            <div className="mt-4 flex gap-3 items-center justify-start">
+                                <motion.div className="text-sm text-gray-400 ml-2 flex items-center gap-2"
+                                    initial={{ y: 60, opacity: 0 }}
+                                    whileInView={{ y: 0, opacity: 1 }}
+                                    viewport={{ once: true }}
+                                    transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1, delay: 0.2 }}
+                                >
+                                    <div className="relative flex h-3.5 w-3.5 items-center justify-center">
+                                        <span className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping duration-300" />
+
+                                        <span className="relative inline-flex size-2 rounded-full bg-green-600" />
+                                    </div>
+                                    30+ clientes ativos
+                                </motion.div>
+                            </div>
+                        </motion.div>
+                    </div>
+                </div>
+            </section>
+
+            {/* LOGO MARQUEE */}
+            <motion.section className="border-y border-white/6 bg-white/1 max-md:mt-10"
+                initial={{ y: 60, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
+            >
+                <div className="max-w-6xl mx-auto px-6">
+                    <div className="w-full overflow-hidden py-6">
+                        <div className="flex gap-14 items-center justify-center animate-marquee whitespace-nowrap">
+                            {trustedLogosText.concat(trustedLogosText).map((logo, i) => (
+                                <span
+                                    key={i}
+                                    className="mx-6 text-sm md:text-base font-semibold text-gray-400 hover:text-gray-300 tracking-wide transition-colors"
+                                >
+                                    {logo}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </motion.section>
+        </>
+    );
+};
